72d7f17cc399143519282f8a53a2511eb93a48d0
trsimanto <simanto14feb@gmail.com>
2023-07-05 04:43:23 +0600

    feat(UITest): Espresso UI test ongoing

--- app/src/androidTest/java/com/towhid/attendance/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/towhid/attendance/ExampleInstrumentedTest.kt
@@ -1,12 +1,18 @@
 package com.towhid.attendance
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import com.android21buttons.fragmenttestrule.FragmentTestRule
+import com.towhid.attendance.fragments.fgSubmission.view.SubmissionFragment
+import org.junit.Assert.*
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -21,4 +27,27 @@ class ExampleInstrumentedTest {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.towhid.attendance", appContext.packageName)
     }
+
+
+    @JvmField
+    @Rule
+    var fragmentTestRule: FragmentTestRule<*, SubmissionFragment> =
+        FragmentTestRule.create(
+            SubmissionFragment::class.java
+        )
+
+    @Test
+    @Throws(Exception::class)
+    fun clickButton() {
+        onView(withId(R.id.et_name))
+            .perform(typeText("user"))
+            .perform(ViewActions.closeSoftKeyboard())
+
+        onView(withId(R.id.et_user_id))
+            .perform(typeText("user_id"))
+            .perform(ViewActions.closeSoftKeyboard())
+
+
+    }
+
 }
